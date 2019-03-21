af03bf42b37f2a0bea18003ca95b7f8736a970de
tongsq19 <tongsq14@mails.tsinghua.edu.cn>
2019-03-21 20:27:17 +0800

    Add unit tests for InputValidator class

--- src/test/java/tw/core/InputValidatorTest.java
+++ src/test/java/tw/core/InputValidatorTest.java
@@ -1,8 +1,32 @@
 package tw.core;
 
+import org.junit.Test;
+import tw.validator.InputValidator;
+
+import static junit.framework.TestCase.assertTrue;
+import static junit.framework.TestCase.assertFalse;
+
 /**
  * 在InputValidatorTest文件中完成InputValidator中对应的单元测试
  */
 public class InputValidatorTest {
+    @Test
+    public void InputValid_Test() {
+        assertTrue(new InputValidator().validate("1 2 3 4"));
+        assertTrue(new InputValidator().validate("5 6 7 8"));
+        assertTrue(new InputValidator().validate("7 8 9 0"));
+
+        assertFalse(new InputValidator().validate("1 1 3 4"));
+        assertFalse(new InputValidator().validate("1 2 1 4"));
+        assertFalse(new InputValidator().validate("1 2 3 1"));
+
+        assertFalse(new InputValidator().validate("10 2 3 4"));
+        assertFalse(new InputValidator().validate("1 20 3 4"));
+        assertFalse(new InputValidator().validate("1 2 30 4"));
+        assertFalse(new InputValidator().validate("1 2 3 40"));
+
+        assertFalse(new InputValidator().validate("1 2- 3 4"));
+        assertFalse(new InputValidator().validate("1 2 3"));
+    }
 
 }
