c33ea4da6b10f44c03d7a382d233bf6a610020af
tongsq19 <tongsq14@mails.tsinghua.edu.cn>
2019-03-21 20:46:01 +0800

    Add unit tests for class RandomIntGenerator

--- src/test/java/tw/core/RandomIntGeneratorTest.java
+++ src/test/java/tw/core/RandomIntGeneratorTest.java
@@ -1,9 +1,39 @@
 package tw.core;
 
 
+import org.junit.Test;
+import tw.core.generator.RandomIntGenerator;
+
+import java.util.Random;
+
+import static org.junit.Assert.assertEquals;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
+
 /**
  * 在RandomIntGeneratorTest文件中完成RandomIntGenerator中对应的单元测试
  */
 public class RandomIntGeneratorTest {
 
+    @Test
+    public void GenerateRandomIntegersTest() {
+        Random mockRand = mock(Random.class);
+        when(mockRand.nextInt(10)).thenReturn(0).thenReturn(9).thenReturn(8).thenReturn(7);
+
+        RandomIntGenerator randomIntGenerator = new RandomIntGenerator();
+        randomIntGenerator.setRandom(mockRand);
+        String result = randomIntGenerator.generateNums(10, 4);
+        assertEquals(result, "0 9 8 7");
+    }
+
+    @Test
+    public void GenerateRandomIntegersWithRepeatTest() {
+        Random mockRand = mock(Random.class);
+        when(mockRand.nextInt(10)).thenReturn(0).thenReturn(9).thenReturn(9).thenReturn(8).thenReturn(7);
+
+        RandomIntGenerator randomIntGenerator = new RandomIntGenerator();
+        randomIntGenerator.setRandom(mockRand);
+        String result = randomIntGenerator.generateNums(10, 4);
+        assertEquals(result, "0 9 8 7");
+    }
 }
